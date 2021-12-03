c54273aab6474ec7c690bb1a58a94ca0506b9432
Mateusz Tatarski <mateusz.tatarski@curiosum.com>
2021-12-03 10:04:03 +0100

    Day2 part1

---
+++ lib/day2.ex
@@ -0,0 +1,28 @@
+defmodule Day2.Part1 do
+  def run() do
+    list = "lib/day2_data.txt" |> File.read!() |> String.trim() |> String.split("\n")
+    #  list =  ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]
+    basic_list = %{horizontal: 0, depth: 0}
+
+    list =
+      list
+      |> Enum.map(fn x ->
+        [command, value] = String.split(x, " ")
+
+        {command, value}
+      end)
+
+    map =
+      Enum.reduce(list, basic_list, fn {command, value}, acc ->
+        value = String.to_integer(value)
+
+        case command do
+          "forward" -> Map.replace(acc, :horizontal, Map.get(acc, :horizontal) + value)
+          "down" -> Map.replace(acc, :depth, Map.get(acc, :depth) + value)
+          "up" -> Map.replace(acc, :depth, Map.get(acc, :depth) - value)
+        end
+      end)
+
+    map.horizontal * map.depth
+  end
+end
